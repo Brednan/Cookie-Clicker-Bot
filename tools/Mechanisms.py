18dda5d3700ece185de24ad1ab86cf896c80efcf
Brendan Caldwell <40771777+Brednan@users.noreply.github.com>
2022-02-15 15:09:40 -0800

    Update Mechanisms.py

--- tools/Mechanisms.py
+++ tools/Mechanisms.py
@@ -1,6 +1,8 @@
 import mss
 from PIL import Image
 import cv2
+import pyautogui
+import numpy as np
 
 class Mechanisms():
     def take_screenshot(self, monitor:int, cache_path):
@@ -17,7 +19,10 @@ class Mechanisms():
                 print('unknown error capturing screen!')
     
     def locate_object(self, template:str):
-        match = cv2.matchTemplate(cv2.imread('./tools/ss_cache/screen.png'), cv2.imread(template), cv2.TM_CCOEFF)
-        loc = cv2.minMaxLoc(match)
-        print(loc[2], loc[3])
-        print(tuple(map(lambda a, b: a - b,loc[2], loc[3])))
+        match = cv2.matchTemplate(cv2.imread('./tools/ss_cache/screen.png', cv2.IMREAD_UNCHANGED), cv2.imread(template, cv2.IMREAD_UNCHANGED), cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
+        threshold = 0.8
+        if max_val >= threshold:
+            pyautogui.moveTo(x=min_loc[0], y=min_loc[1])
+
+        
