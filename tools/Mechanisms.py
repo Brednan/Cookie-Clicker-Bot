806c7e6b7f2f6a5bfb80a7c658e6b1fbcda62fff
Brendan Caldwell <40771777+Brednan@users.noreply.github.com>
2022-02-16 13:45:25 -0800

    Update Mechanisms.py

--- tools/Mechanisms.py
+++ tools/Mechanisms.py
@@ -7,24 +7,26 @@ import numpy as np
 class Mechanisms():
     def take_screenshot(self, monitor:int, cache_path):
         with mss.mss() as mss_instance:
-            try:
-                selected_monitor = mss_instance.monitors[monitor]
-                screenshot = mss_instance.grab(monitor=selected_monitor)
+            selected_monitor = mss_instance.monitors[monitor]
+            screenshot = mss_instance.grab(monitor=selected_monitor)
 
-                img = Image.frombytes('RGB', screenshot.size, screenshot.bgra, 'raw', 'BGRX')
-                img.save(fp=cache_path, format='PNG')
-            except IndexError:
-                print('Monitor not found!')
-            except:
-                print('unknown error capturing screen!')
+            img = Image.frombytes('RGB', screenshot.size, screenshot.bgra, 'raw', 'BGRX')
+            img.save(fp=cache_path, format='PNG')
+
     
-    def locate_object(self, template:str):
+    def locate_object(self, template:str, threshold):
         match = cv2.matchTemplate(cv2.imread('./tools/ss_cache/screen.png'), cv2.imread(template), cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
         threshold = 0.8
+        temp_shape = cv2.imread(template).shape
 
         if max_val >= threshold:
-            pyautogui.moveTo(x=min_loc[0], y=min_loc[1])
+            coordinates = max_loc
+            coordinates = tuple(map(lambda a, b: a - b, coordinates, temp_shape[0:2]))
+            return (coordinates)
 
-        temp_shape = cv2.imread(template).shape[0]
-        print(temp_shape)
+        else:
+            return None
+    
+    def click_object(self, location):
+        pyautogui.moveTo(x=location[0], y=location[1])
